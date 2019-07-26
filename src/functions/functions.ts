ed618216be16bf543e517eece4076f31999828be
Daniel G. Wilson <daniel@danielgwilson.com>
2019-07-26 11:27:43 -0700

    Removed in progress function

--- src/functions/functions.ts
+++ src/functions/functions.ts
@@ -12,13 +12,13 @@ async function issueCountForRepo(repo: string, params?: string[]) {
   return await searchIssuesForRepo(repo, params);
 }
 
-/**
- * Gets the number of issues for a particular repo given some search parameters, also adding a hyperlink to the corresponding repo (filtered).
- * @customfunction
- * @param repo
- * @param params Search parameters
- * @returns The number of issues and a hyperlink to the repo (filtered).
- */
-async function issueCountForRepoWithHyperlink(repo: string, params?: string[]) {
-  return `=HYPERLINK(https://google.com)`;
-}
+// /**
+//  * Gets the number of issues for a particular repo given some search parameters, also adding a hyperlink to the corresponding repo (filtered).
+//  * @customfunction
+//  * @param repo
+//  * @param params Search parameters
+//  * @returns The number of issues and a hyperlink to the repo (filtered).
+//  */
+// async function issueCountForRepoWithHyperlink(repo: string, params?: string[]) {
+//   return `=HYPERLINK(https://google.com)`;
+// }
